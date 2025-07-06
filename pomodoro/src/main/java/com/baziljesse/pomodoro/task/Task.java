a154367c342d25b890d5e27968dfa2ed09dc79b7
JesseBarnett24 <messijessi58@gmail.com>
2025-07-06 22:05:55 +1000

    typo

--- pomodoro/src/main/java/com/baziljesse/pomodoro/task/Task.java
+++ pomodoro/src/main/java/com/baziljesse/pomodoro/task/Task.java
@@ -1,6 +1,20 @@
 package com.baziljesse.pomodoro.task;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Task {
+    @Id
+    @SequenceGenerator(
+            name="task_sequence",
+            sequenceName = "task_sequence",
+            allocationSize=1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "task_sequence"
+    )
     private Long id;
     private String name;
     private String description;
